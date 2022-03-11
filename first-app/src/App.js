c9bd81c73757327fd3d6a1bd59db041ec8ec0fc2
Saheb Dhara <sahebdhara961@gmail.com>
2022-03-11 18:22:46 +0530

    h1

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+// import React from 'react'
 
-const App = () => {
-  return (
-    <div> Hello World</div>
+const App = () => { 
+  return  (
+  <h1> hello world</h1>
   )
 }
 
