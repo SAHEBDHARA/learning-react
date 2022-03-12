da79a45925c39881c0ac202eece7b29c56808443
Saheb Dhara <sahebdhara961@gmail.com>
2022-03-12 08:41:55 +0530

    first commit in jsx

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,20 @@
-// import React from 'react'
+import React from 'react'
 
-const App = () => { 
-  return  (
-  <h1> hello world</h1>
-  )
-}
+
+const App = () =>{
+  return (
+     <div>
+       <h1> hello world</h1>
+     </div>
+   )
+ }
+ // how actually react look like 
+// const App = () => { 
+//   return  (
+//   // <h1> hello world</h1>
+//   React.createElement("h1",{},"hello world")
+//   )
+// }
 
 export default App
 
